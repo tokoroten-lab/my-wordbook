445e1d485e869b222584212f5fe2a4a8ca4e523e
tokoroten-lab <tokoroten.lab@gmail.com>
2020-04-11 20:04:48 +0900

    Create the project

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {}
+})
+
+export default App;
