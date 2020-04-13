da664d3ecb26b8eb82e4ba34a1ed00d325cfe146
tokoroten-lab <tokoroten.lab@gmail.com>
2020-04-13 20:44:14 +0900

    Upgrade Word model

--- models/Word.ts
+++ models/Word.ts
@@ -1,6 +1,8 @@
 /// <reference types="realm" />
 
-class Word {
+import IModelData from './interfaces/IModelData'
+
+class Word implements IModelData {
   public static schema: Realm.ObjectSchema = {
     name: 'Word',
     properties: {
@@ -9,7 +11,13 @@ class Word {
     }
   }
 
-  constructor() {}
+  public readonly raw: string
+  public readonly normal: string
+
+  constructor(raw: string, normal: string) {
+    this.raw = raw
+    this.normal = normal
+  }
 }
 
 export default Word
