d6c7275afb1fc6a0b94858a9b2d68271891f982d
tokoroten-lab <tokoroten.lab@gmail.com>
2020-04-14 01:28:21 +0900

    Upgrade Document model

--- models/Document.ts
+++ models/Document.ts
@@ -1,8 +1,8 @@
 /// <reference types="realm" />
 
-import nlp from 'compromise'
 import IModelData from './interfaces/IModelData'
 import Normalizer from './utils/Normalizer'
+import Paragraph from './Paragraph'
 
 class Document implements IModelData {
   public static schema: Realm.ObjectSchema = {
@@ -16,10 +16,12 @@ class Document implements IModelData {
 
   public readonly raw: string
   public readonly normal: string
+  public readonly paragraphs: Paragraph[]
 
   constructor(document: string) {
     this.raw = document
     this.normal = Normalizer.normalize(document)
+    this.paragraphs = Paragraph.getParagraphs(this.raw)
   }
 }
 
