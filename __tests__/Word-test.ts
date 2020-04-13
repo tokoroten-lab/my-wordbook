ceebb39c5fdcaad20bf6d4862dbb13227340b4c6
tokoroten-lab <tokoroten.lab@gmail.com>
2020-04-14 02:24:46 +0900

    Add a test of Word model

--- __tests__/Word-test.ts
+++ __tests__/Word-test.ts
@@ -1,9 +1,30 @@
 import Word from "../models/Word";
 
-describe('Normalize document', () => {
-  test('normalize', () => {
+describe('Word model unit test', () => {
+  test('normalize word', () => {
     const normal = new Word('This', 'this').normal;
 
     expect(normal).toBe('this');
+  });
+
+  test('split words', () => {
+    const testDocument = `I'm a developer of this application.`;
+    const expectResults = [
+      `I`,
+      'am',
+      `a`,
+      `developer`,
+      `of`,
+      `this`,
+      `application`
+    ];
+
+    const words = Word.getWords(testDocument);
+
+    expect(words.length).toBe(expectResults.length);
+
+    for(let i = 0; i < words.length; ++i) {
+      expect(words[i].raw).toBe(expectResults[i]);
+    }
   });
 });
