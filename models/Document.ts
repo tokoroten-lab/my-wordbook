61a79a885541d7349857cf4525c76415387ab5bc
tokoroten-lab <tokoroten.lab@gmail.com>
2020-04-13 20:04:33 +0900

    Use Normalizer util

--- models/Document.ts
+++ models/Document.ts
@@ -2,6 +2,7 @@
 
 import nlp from 'compromise'
 import IModelData from './interfaces/IModelData'
+import Normalizer from './utils/Normalizer'
 
 class Document implements IModelData {
   public static schema: Realm.ObjectSchema = {
@@ -22,21 +23,7 @@ class Document implements IModelData {
   }
 
   private static normalize(document: string): string {
-    const doc = nlp(document)
-    doc.normalize({
-      whitespace: true,
-      case: false,
-      punctuation: false,
-      uncode: false,
-      contractions: true,
-      acronyms: true,
-      parentheses: false,
-      possessives: false,
-      plurals: false,
-      verbs: false,
-      honorifics: false
-    })
-    return doc.out('text')
+    return Normalizer.normalize(document)
   }
 }
 
