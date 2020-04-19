cfd1219d3693b6b41a563815036405144ce74d92
tokoroten-lab <tokoroten.lab@gmail.com>
2020-04-19 20:48:17 +0900

    Add stock() method to ModelManager

--- models/ModelManager.ts
+++ models/ModelManager.ts
@@ -1,8 +1,17 @@
+import Realm from 'realm';
+import Document from './Document';
+import Pragraph from './Paragraph';
+import Sentence from './Sentence';
+import Word from './Word';
+
 class ModelManager {
   private static _instance: ModelManager;
+  private readonly realm: Realm;
 
   private constructor() {
-    //
+    this.realm = new Realm({
+      schema: [Document.schema, Pragraph.schema, Sentence.schema, Word.schema],
+    });
   }
 
   public static get instance() {
@@ -12,6 +21,13 @@ class ModelManager {
 
     return ModelManager._instance;
   }
+
+  public stock(document: Document) {
+    this.realm.write(() => {
+      const stockedDocument = this.realm.create('Document', document.json);
+      console.log(stockedDocument);
+    });
+  }
 }
 
-export default ModelManager;
+export default ModelManager.instance;
