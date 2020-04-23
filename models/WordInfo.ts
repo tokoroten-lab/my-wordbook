b0f884b036a51eefa71547b44875f5852a6f7fb4
tokoroten-lab <tokoroten.lab@gmail.com>
2020-04-23 23:48:29 +0900

    Add 'count' property to WordInfo model

--- models/WordInfo.ts
+++ models/WordInfo.ts
@@ -4,6 +4,10 @@ const WordInfoSchema: Realm.ObjectSchema = {
   name: 'WordInfo',
   properties: {
     normal: 'string',
+    count: {
+      type: 'int',
+      default: 0,
+    },
     recognitionLevel: {
       type: 'int',
       default: 0,
