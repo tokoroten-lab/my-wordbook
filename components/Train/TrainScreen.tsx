1bcd035f4d34f87eae9aa6381828258be5e65d8b
tokoroten-lab <tokoroten.lab@gmail.com>
2020-04-28 19:42:39 +0900

    Create the base of train screen

--- components/Train/TrainScreen.tsx
+++ components/Train/TrainScreen.tsx
@@ -1,13 +1,71 @@
 import React from 'react';
-import {StyleSheet, SafeAreaView} from 'react-native';
+import {StyleSheet, SafeAreaView, Text, Button} from 'react-native';
 
 function TrainScreen() {
-  return <SafeAreaView style={styles.container} />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.wordInfo}>
+        <Text style={styles.word}>Word</Text>
+        <Text style={styles.evaluation}>123.456789</Text>
+      </SafeAreaView>
+      <SafeAreaView style={styles.buttonGroup}>
+        <SafeAreaView style={styles.button}>
+          <Button
+            title="Complete"
+            color="#d2691e"
+            accessibilityLabel="Complete"
+            onPress={() => console.log('complete')}
+          />
+        </SafeAreaView>
+        <SafeAreaView style={styles.button}>
+          <Button
+            title="Good"
+            color="#805c5c"
+            accessibilityLabel="Good"
+            onPress={() => console.log('good')}
+          />
+        </SafeAreaView>
+        <SafeAreaView style={styles.button}>
+          <Button
+            title="Bad"
+            color="#5f9ea0"
+            accessibilityLabel="Good"
+            onPress={() => console.log('bad')}
+          />
+        </SafeAreaView>
+        <SafeAreaView style={styles.button}>
+          <Button
+            title="Incomplete"
+            color="#483d8b"
+            accessibilityLabel="Incomplete"
+            onPress={() => console.log('incomplete')}
+          />
+        </SafeAreaView>
+      </SafeAreaView>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    padding: 16,
+  },
+  wordInfo: {
+    alignItems: 'center',
+    marginVertical: 32,
+  },
+  word: {
+    fontSize: 24,
+  },
+  evaluation: {
+    fontSize: 18,
+    color: '#dc143c',
+  },
+  buttonGroup: {},
+  button: {
+    paddingVertical: 4,
   },
 });
 
